be56ac3dca2d92d03558b42264389fcf53a132f0
kongphengta <kong.vixay@gmail.com>
2024-03-25 21:12:49 +0100

    Add Modal

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/omelette", "omelette", "/pages/omeletteauxepinards.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
