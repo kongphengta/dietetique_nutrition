98dc313c0d31571c0a37b291d62ca5fea5fcdcb1
kongphengta <kong.vixay@gmail.com>
2024-03-25 08:38:55 +0100

    Add Forms

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,10 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/omelette", "omelette", "/pages/omeletteauxepinards.html"),];
+    new Route("/omelette", "omelette", "/pages/omeletteauxepinards.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Diététique & Nutrition";
